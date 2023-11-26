d0344c8d87dfbbcf4933fd4bedda56f6d8619bfc
Ziad Mahmoud <ziyadmahmoud63@gmail.com>
2023-11-26 20:12:25 +0200

    At this point does it even matter

--- JS/script.js
+++ JS/script.js
@@ -55,6 +55,18 @@ var keplerImageIndex = 0;
 
 /*********************************************************************************/
 
+const telescopeImages = [
+    "../Resources/Hubble/_111493221_s125e012033_1.jpg",
+    "../Resources/JamesWebb/jwst_poster02.jpg",
+    "../Resources/ChandraX-ray/Wanjek_Winter22_02_2019Athena_MO.jpg.webp",
+    "../Resources/Spitzer/hd_spitzer_side_visible.webp",
+    "../Resources/Kepler/Kepler_K2_640x480.jpg"
+];
+
+var telescopeImageIndex = 0;
+
+/*********************************************************************************/
+
 // Functions for Hubble telescope image switching
 
 function updateHubbleBackground() {
@@ -169,6 +181,29 @@ const KeplerNextButton = document.getElementById('KeplerNextButton');
 KeplerNextButton.addEventListener('click', () => {
     keplerImageIndex = (keplerImageIndex + 1) % keplerImages.length;
     updateKeplerBackground();
+});
+
+/*********************************************************************************/
+
+// Functions for Telescope image switching
+
+function updateTelescopeBackground() {
+    const imageUrl = telescopeImages[telescopeImageIndex];
+    document.getElementById('TelescopesDiv').style.backgroundImage = `url(${imageUrl})`;
+}
+
+updateTelescopeBackground();
+
+const TelescopePrevButton = document.getElementById('TelescopesPrevButton');
+TelescopePrevButton.addEventListener('click', () => {
+    telescopeImageIndex = (telescopeImageIndex - 1 + telescopeImages.length) % telescopeImages.length;
+    updateTelescopeBackground();
+});
+
+const TelescopeNextButton = document.getElementById('TelescopesNextButton');
+TelescopeNextButton.addEventListener('click', () => {
+    telescopeImageIndex = (telescopeImageIndex + 1) % telescopeImages.length;
+    updateTelescopeBackground();
 });
 
 /*********************************************************************************/
